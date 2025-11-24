6da183c368b85d73404a8cd2dd684a07a99f0b51
Omar <ohjimenezd@gmail.com>
2025-11-23 18:54:31 -0600

    Agregar id al form en registrar personal

--- frontend/registro-personal.php
+++ frontend/registro-personal.php
@@ -58,7 +58,7 @@ $seccion = 'Registro de personal';
 
                 <h2 class="text-center mb-4" style="color:#4a1026;">Registro de Personal</h2>
 
-                <form action="#" method="POST">
+                <form id="formRegistro" action="#" method="POST">
 
                     <div class="row mb-3">
                         <div class="col-md-6">
